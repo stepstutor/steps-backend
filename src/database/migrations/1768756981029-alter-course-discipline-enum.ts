c17fb455bfd779ee5e6d74160d61a11bc1400585
Sohail Rauf <m_sohail10@live.com>
2026-01-18 22:44:55 +0500

    removed steps_db from migration

--- src/database/migrations/1768756981029-alter-course-discipline-enum.ts
+++ src/database/migrations/1768756981029-alter-course-discipline-enum.ts
@@ -5,41 +5,39 @@ export class AlterCourseDisciplineEnum1768756981029 implements MigrationInterfac
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TYPE "steps_db"."course_discipline_enum" RENAME TO "course_discipline_enum_old"`,
+      `ALTER TYPE "public"."course_discipline_enum" RENAME TO "course_discipline_enum_old"`,
     );
     await queryRunner.query(
-      `CREATE TYPE "steps_db"."course_discipline_enum" AS ENUM('BIOLOGY', 'CHEMISTRY', 'COMPUTER_AND_DATA_SCIENCE', 'MATHEMATICS', 'PHYSICS', 'ENGINEERING', 'EARTH_AND_ENVIRONMENTAL_SCIENCES')`,
+      `CREATE TYPE "public"."course_discipline_enum" AS ENUM('BIOLOGY', 'CHEMISTRY', 'COMPUTER_AND_DATA_SCIENCE', 'MATHEMATICS', 'PHYSICS', 'ENGINEERING', 'EARTH_AND_ENVIRONMENTAL_SCIENCES')`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "discipline" DROP DEFAULT`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "discipline" DROP DEFAULT`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "discipline" TYPE "steps_db"."course_discipline_enum" USING "discipline"::"text"::"steps_db"."course_discipline_enum"`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "discipline" TYPE "public"."course_discipline_enum" USING "discipline"::"text"::"public"."course_discipline_enum"`,
     );
+    await queryRunner.query(`DROP TYPE "public"."course_discipline_enum_old"`);
     await queryRunner.query(
-      `DROP TYPE "steps_db"."course_discipline_enum_old"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "courseType" DROP DEFAULT`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "courseType" DROP DEFAULT`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "courseType" SET DEFAULT 'General'`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "courseType" SET DEFAULT 'General'`,
     );
     await queryRunner.query(
-      `CREATE TYPE "steps_db"."course_discipline_enum_old" AS ENUM('Biology', 'Chemistry', 'Computer & Data Science', 'Earth & Environmental Sciences', 'Engineering', 'Mathematics', 'Physics')`,
+      `CREATE TYPE "public"."course_discipline_enum_old" AS ENUM('Biology', 'Chemistry', 'Computer & Data Science', 'Earth & Environmental Sciences', 'Engineering', 'Mathematics', 'Physics')`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "discipline" TYPE "steps_db"."course_discipline_enum_old" USING "discipline"::"text"::"steps_db"."course_discipline_enum_old"`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "discipline" TYPE "public"."course_discipline_enum_old" USING "discipline"::"text"::"public"."course_discipline_enum_old"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "discipline" SET DEFAULT 'Chemistry'`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "discipline" SET DEFAULT 'Chemistry'`,
     );
-    await queryRunner.query(`DROP TYPE "steps_db"."course_discipline_enum"`);
+    await queryRunner.query(`DROP TYPE "public"."course_discipline_enum"`);
     await queryRunner.query(
-      `ALTER TYPE "steps_db"."course_discipline_enum_old" RENAME TO "course_discipline_enum"`,
+      `ALTER TYPE "public"."course_discipline_enum_old" RENAME TO "course_discipline_enum"`,
     );
   }
 }
