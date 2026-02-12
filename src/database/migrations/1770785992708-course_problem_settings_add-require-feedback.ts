69152b1fc74b0d38e8c38287e939ded2bcecdc78
Sohail Rauf <m_sohail10@live.com>
2026-02-12 19:02:39 +0500

    Fixed migration

--- src/database/migrations/1770785992708-course_problem_settings_add-require-feedback.ts
+++ src/database/migrations/1770785992708-course_problem_settings_add-require-feedback.ts
@@ -5,19 +5,19 @@ export class Migrations1770785992708 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course_problem_settings" ADD "requireFeedback" boolean NOT NULL DEFAULT false`,
+      `ALTER TABLE "public"."course_problem_settings" ADD "requireFeedback" boolean NOT NULL DEFAULT false`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "courseType" SET NOT NULL`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "courseType" SET NOT NULL`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course" ALTER COLUMN "courseType" DROP NOT NULL`,
+      `ALTER TABLE "public"."course" ALTER COLUMN "courseType" DROP NOT NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."course_problem_settings" DROP COLUMN "requireFeedback"`,
+      `ALTER TABLE "public"."course_problem_settings" DROP COLUMN "requireFeedback"`,
     );
   }
 }
