e749c7370447afdb99d9458b8a9f60ee28da0525
Sohail Rauf <m_sohail10@live.com>
2026-02-13 18:01:27 +0500

    Fix migration

--- src/database/migrations/1769695487808-alter-problem-add-cowrite_attempts.ts
+++ src/database/migrations/1769695487808-alter-problem-add-cowrite_attempts.ts
@@ -5,19 +5,19 @@ export class AlterProblemAddCowriteAttempts1769695487808 implements MigrationInt
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."problem" ADD "cowriteProblemAttempts" integer NOT NULL DEFAULT '0'`,
+      `ALTER TABLE "public"."problem" ADD "cowriteProblemAttempts" integer NOT NULL DEFAULT '0'`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."problem" ADD "cowriteSolutionAttempts" integer NOT NULL DEFAULT '0'`,
+      `ALTER TABLE "public"."problem" ADD "cowriteSolutionAttempts" integer NOT NULL DEFAULT '0'`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."problem" DROP COLUMN "cowriteSolutionAttempts"`,
+      `ALTER TABLE "public"."problem" DROP COLUMN "cowriteSolutionAttempts"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "steps_db"."problem" DROP COLUMN "cowriteProblemAttempts"`,
+      `ALTER TABLE "public"."problem" DROP COLUMN "cowriteProblemAttempts"`,
     );
   }
 }
